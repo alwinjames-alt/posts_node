32ce35a7b4c67a9331e93b9fe16f57accea652c8
Alwin James <alwin.james@thebluespire.com>
2025-09-30 17:16:34 +0530

    added k8s files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+// Load environment variables
+dotenv.config();
 
 const { getStoredPosts, storePosts } = require('./data/posts');
 
